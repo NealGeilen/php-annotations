99923a4f99742bb50b1eec1d677f77667db6e631
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-04-13 12:57:36 +0200

    Json response

--- routing/Response/JsonResponse.php
+++ routing/Response/JsonResponse.php
@@ -1,1 +1,21 @@
 <?php
+namespace Router\Response;
+
+class JsonResponse implements ResponseEnum {
+
+    protected $object = [];
+    protected $http_response_code = 200;
+
+    public function __construct($object = [], $http_response_code = 200)
+    {
+        $this->object = $object;
+        $this->http_response_code = $http_response_code;
+    }
+
+    public function display(): void
+    {
+        http_response_code($this->http_response_code);
+        echo json_encode($this->object);
+    }
+
+}
